e73bf060a036af103d703e1866ad1f9dbef78567
Michael Arakelyan <87676346+CringeStar@users.noreply.github.com>
2024-03-27 09:46:52 -0700

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/ClimberSubsystem.java
+++ src/main/java/frc/robot/ClimberSubsystem.java
@@ -1,1 +1,21 @@
+package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+public class AmpSubsystem extends SubsystemBase {
+
+
+    private WPI_VictorSPX motor;
+
+    public AmpSubsystem() {
+        motor = new WPI_VictorSPX(16);
+    }
+
+    public void climb() {
+        motor.set(ControlMode.PercentOutput, 1);
+        Thread.sleep(10000); //assuming it takes 10 seconds fot climber to finish
+        motor.set(ControlMode.PercentOutput, 1);
+    }
+}
