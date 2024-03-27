04df4537d3280af479ce77ba72de523c3e79576b
Michael Arakelyan <87676346+CringeStar@users.noreply.github.com>
2024-03-27 08:43:21 -0700

    Update AmpSubsystem.java

--- src/main/java/frc/robot/Amp/AmpSubsystem.java
+++ src/main/java/frc/robot/Amp/AmpSubsystem.java
@@ -2,15 +2,15 @@ package frc.robot.Amp;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class AmpSubsystem extends SubsystemBase {
 
 
-    private VictorSPX bottomMotor;
+    private WPI_VictorSPX bottomMotor;
 
     public AmpSubsystem() {
-        bottomMotor = new VictorSPX(15);
+        bottomMotor = new WPI_VictorSPX(15);
     }
 
     public void shoot(boolean direction, boolean onOff) {
